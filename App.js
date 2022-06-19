3eaf72ec3129b0a7b5cb7bd752f1d77b738164f5
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-06-19 10:11:29 +0200

    flatlist

--- App.js
+++ App.js
@@ -6,127 +6,99 @@ import {
   View,
   Button,
   Linking,
+  ScrollView,
+  SafeAreaView,
+  RefreshControl,
+  FlatList,
 } from 'react-native';
 
 const App /*: () => Node */ = () => {
-  const [name, setName] = useState("Nieznajomy")
-  const [counter, setCounter] = useState({ name: "Licznik 1", value: 0 })
-  const [buttonClicked, setButtonClicked] = useState(false);
 
-  const buttonClick = () => {
-    setButtonClicked(true)
-    Linking.openURL('https://teb.pl')
-  }
+  const [data, setData] = useState([
+    { key: 1534, row: "Wiersz 1" },
+    { key: 2454, row: "Wiersz 2" },
+    { key: 3435, row: "Wiersz 3" },
+    { key: 4543, row: "Wiersz 4" },
+    { key: 435, row: "Wiersz 5" },
+    { key: 6453, row: "Wiersz 6" },
+    { key: 745, row: "Wiersz 7" },
+    { key: 3458, row: "Wiersz 8" },
+    { key: 9345, row: "Wiersz 9" },
+    { key: 13450, row: "Wiersz 10" },
+  ])
 
-  const changeNameState = () => {
-    setName("Paweł")
-  }
+  const [listRefreshing, setListRefreshing] = useState(false)
 
-  const incrementCounter = () => {
-    /* Zadanie zaliczeniowe 1 : zmodyfikuj funkcję tak, żeby po każdym
-      kliknięciu wartość licznika wzrasała o 1 */
-    setCounter({ name: "Licznik 1", value: 1 })
+  const onListRefresh = () => {
+    setListRefreshing(true)
+    setData([...data, { key: 34511, row: "Wiersz 11" }])
+    setListRefreshing(false)
   }
 
   return (
-    <View style={styles.mainView}>
-      <Text style={styles.text}>Cześć {name}</Text>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.buttonView}>
-          <Button
-            style={styles.button}
-            title="Kliknij mnie!"
-            onPress={buttonClick}
-          ></Button>
-        </View>
-        <View style={styles.buttonView}>
-          <Button
-            style={styles.button}
-            title="Zmień stan"
-            onPress={changeNameState}
-          ></Button>
-        </View>
+    <SafeAreaView style={styles.appMain}>
+      <FlatList
+        // horizontal
+        refreshControl=
+        {<RefreshControl
+          refreshing={listRefreshing}
+          onRefresh={onListRefresh}
+        />}
+        data={data}
+        keyExtractor={(item, index) => index}
+        renderItem={({item, index}) => (
+          <View style={styles.row} >
+                <Text style={styles.text}>(Index:{index}){'\n'}{item.row}{'\n'}(Klucz:{item.key})</Text>
+          </View>
+        )}
+      />
+      {/* <ScrollView
+        horizontal={false}
+        refreshControl=
+        {<RefreshControl
+          refreshing={listRefreshing}
+          onRefresh={onListRefresh}
+        />}
+      >
+        {
+          data.map((row) => {
+            return (
+              <View style={styles.row} key={row.key}>
+                <Text style={styles.text}>{row.row}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView> */}
+      {/* <View style={styles.row}>
+        <Text>Item 1</Text>
       </View>
-
-      <Text style={styles.text}>{counter.name}: {counter.value}</Text>
-      <View style={styles.buttonView}>
-        <Button
-          style={styles.button}
-          title="Zwiększ licznik"
-          onPress={incrementCounter}
-        ></Button>
+      <View style={styles.row}>
+        <Text>Item 2</Text>
       </View>
-      <Text style={styles.text}>
-        {buttonClicked ? "Guzik został wcisnięty" : "Guzik nie został wciśnięty"}
-      </Text>
-      <View style={styles.flexDemoContainer}>
-        <View style={{ flex: 1 }}>
-          <Text style={styles.flexDemoColumn}>1</Text>
-        </View>
-        <View style={{ flex: 2 }}>
-          <Text style={styles.flexDemoColumn}>2</Text>
-        </View>
-        <View style={{ flex: 1 }}>
-          <Text style={styles.flexDemoColumn}>3</Text>
-        </View>
-      </View>
-      <View style={styles.flexDemoContainer2}>
-        <View style={{ flex: 1 }}>
-          <Text style={styles.flexDemoRow}>1</Text>
-        </View>
-        <View style={{ flex: 2 }}>
-          <Text style={styles.flexDemoRow}>2</Text>
-        </View>
-        <View style={{ flex: 3 }}>
-          <Text style={styles.flexDemoRow}>3</Text>
-        </View>
-      </View>
-    </View>
+      <View style={styles.row}>
+        <Text>Item 3</Text>
+      </View> */}
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  mainView: {
-    backgroundColor: '#b3d4fc',
+  appMain: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#387094',
+  },
+  row: {
+    backgroundColor: '#60a4d1',
     justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    padding: 20,
+    
   },
   text: {
-    backgroundColor: '#387094',
     color: '#ffffff',
-    width: '80%',
-    height: 30,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    margin: 10,
-  },
-  flexDemoContainer: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-  },
-  flexDemoContainer2: {
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    width: '100%',
-    height: 400,
-  },
-  flexDemoColumn: {
-    backgroundColor: '#387094',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    margin: 5,
-  },
-  flexDemoRow: {
-    backgroundColor: '#387094',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    flex: 1,
-    margin: 5,
-  },
-  buttonView: {
-    margin: 10,
-  },
+  }
 });
 
 export default App;
